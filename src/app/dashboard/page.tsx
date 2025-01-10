74645fffa32ddb5c7f269eb289682d1226d6ccff
Stefan Kuehne <s.kuehne@sk-online-marketing.de>
2025-01-10 11:44:54 +0100

    Implementierung der Dashboard-Seite hinzugefügt

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,31 @@
+// src/app/clubs/page.tsx
+import React from "react";
+import { getGolfClubs } from "@/lib/sanity/getGolfClubs";
+import NavigationFrontend from "@/components/frontend-ui/NavigationFrontend";
+import FooterFrontend from "@/components/frontend-ui/FooterFrontend";
+import FilterableClubGrid from "@/components/clubs/FilterableClubGrid";
+
+const ClubsPage = async () => {
+    // Daten aus Sanity CMS laden (auf Server-Seite)
+    const clubs = await getGolfClubs();
+
+    return (
+        <>
+            {/* Navigation */}
+            <NavigationFrontend />
+
+            {/* FilterableClubGrid */}
+            <div className="container mx-auto px-0 py-6 lg:px-6 xl:px-8">
+                {/* Übergabe der initialen Clubs-Daten ohne initiale Filter */}
+                <FilterableClubGrid
+                    initialClubs={clubs}
+                />
+            </div>
+
+            {/* Footer */}
+            <FooterFrontend />
+        </>
+    );
+};
+
+export default ClubsPage;
