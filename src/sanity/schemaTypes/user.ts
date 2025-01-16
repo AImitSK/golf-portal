96dc9f3bb62bcd3b28c839f417a55c548ecf58be
Stefan Kuehne <s.kuehne@sk-online-marketing.de>
2025-01-16 10:28:14 +0100

    Rename schema from 'golfUser' to 'user'
    Updated the schema name to 'user' for consistency and clarity. This change simplifies the naming convention and aligns it with general best practices. Other schema properties remain unchanged.

--- src/sanity/schemaTypes/user.ts
+++ src/sanity/schemaTypes/user.ts
@@ -2,7 +2,7 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { ValidationRule } from '@/types/sanity'
 
 const schema: SchemaTypeDefinition = {
-    name: 'golfUser',
+    name: 'user',
     title: 'Golf User',
     type: 'document',
     fields: [
